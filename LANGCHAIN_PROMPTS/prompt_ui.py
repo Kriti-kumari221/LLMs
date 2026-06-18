ed436fc468b626933655ab2a8c261d6879fc8be2
Kriti-kumari221 <kriticse005@gmail.com>
2026-06-18 15:16:48 +0530

    Prompts added

---
+++ LANGCHAIN_PROMPTS/prompt_ui.py
@@ -0,0 +1,24 @@
+import streamlit as st 
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFacePipeline, ChatHuggingFace
+from dotenv import load_dotenv
+load_dotenv()
+llm = HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs={
+        "max_new_tokens": 100,
+        "temperature": 0.5
+    }
+)
+
+model = ChatHuggingFace(llm=llm)
+# model = ChatHuggingFace(llm=llm)
+st.header("Reserch Tool")
+user_input=st.text_input("Enter your prompt")
+if st.button("Summarize"):
+    result=model.invoke(user_input)
+    st.write(result.content)
+
+
+
