ca52efc48e64239e6a75cdf0969742f6ebe535be
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2026-08-30 11:59:02 +0530

    Add Patient model with email validation and name transform
    Implement a Patient model with validation for email and name transformation.

---
+++ 6Output_parsers/field_validator.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
+from typing import List,Dict,Optional,Annotated
+class Patient(BaseModel):
+    name:str
+    email:EmailStr
+    age:int
+    weight:float
+    married:bool
+    allergies:List[str]
+    contact_details:Dict[str,str]
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls,value):
+        valid_domains=['hdfc.com', 'icsc.com']
+        domain_name=value.split('@')[-1]
+        if domain_name not in valid_domains:
+            raise ValueError("Not a valid domain ")
+        return value
+    @field_validator('name')
+    @classmethod
+    def transform_name(cls,value):
+        return value.upper()
+def update_patient_data(patient:Patient):
+    print(patient.name)
+    print(patient.age)
+    print(patient.married)
+patient_info={'name':'kriti','email':'abc@hdfc.com','linkedin_url':'https://linkedin.com/123','age':30,'weight':75.2,'married':True,'allergies':['pollen','dust'],'contact_details':{'phone':342342}}
+patient1=Patient(**patient_info)
+update_patient_data(patient1)
+    
